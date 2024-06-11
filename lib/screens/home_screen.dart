00a50d8a2f8da85c4093d0fd0049894f4877c314
seyoo630 <32202841@dankook.ac.kr>
2024-06-12 02:19:27 +0900

    logout added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart'; // 카카오 SDK import
 import 'tabs/time_running_tab.dart';
 import 'tabs/distance_goal_tab.dart';
 import 'tabs/pace_check_tab.dart';
 import '../widgets/bottom_icon_section.dart'; // 추가
+import 's_login.dart'; // 로그인 화면 import 추가
 
 class HomeScreen extends StatelessWidget {
+  Future<void> _logout(BuildContext context) async {
+    try {
+      await UserApi.instance.logout();
+      print('로그아웃 성공, SDK에서 토큰 삭제');
+    } catch (error) {
+      print('로그아웃 실패, SDK에서 토큰 삭제 $error');
+    }
+    try {
+      await UserApi.instance.unlink();
+      print('연결 끊기 성공, SDK에서 토큰 삭제');
+    } catch (error) {
+      print('연결 끊기 실패 $error');
+    }
+
+    // 로그인 화면으로 이동
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+          (Route<dynamic> route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +41,25 @@ class HomeScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(
                     top: 40, left: 20, right: 20, bottom: 0), // 필요한 경우 패딩 조정
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // 좌우 정렬
                   children: [
-                    IconButton(
-                      icon: Icon(Icons.home, size: 35),
-                      onPressed: () {},
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.home, size: 35),
+                          onPressed: () {},
+                        ),
+                        Text(
+                          '홈',
+                          style: TextStyle(
+                            fontSize: 23,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      '홈',
-                      style: TextStyle(
-                        fontSize: 23,
-                      ),
+                    IconButton(
+                      icon: Icon(Icons.logout),
+                      onPressed: () => _logout(context),
                     ),
                   ],
                 ),
